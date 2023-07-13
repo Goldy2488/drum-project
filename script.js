4e1944f61c001618a1f954ec0b54b0610d49818b
Goldy2488 <goldy22@navgurukul.org>
2023-07-13 15:29:14 +0530

    last commit

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-let drum= document.getElementById("container")
-drum.addEventListener("Click",(e)=>{
+let arrDrum= document.getElementById("container");
+// console.log(arrDrum)
+
+arrDrum.addEventListener("click",(e)=>{
     let target=e.target.innerHTML;
-    creatSound(target);//for sound
+    // console.log(target)
+    createSound(target);
+    createAnimation(target);
 })
 
 
-function createSound(a){
-    document.getElementsByClassName(a);
+arrDrum.addEventListener("keydown",(e)=>{
+    let target=e.key;
+    
+    let arrkey=["a","s","d","f","j","k","l"];
+    if (!arrkey.includes(target)) {
+        alert("Wrong Key Press");
+        return;}
+    // console.log(target)
+    createSound(target);
+    createAnimation(target);
+});
+
+const createSound=(key)=>{
+    let sound=new Audio(`sounds/${key}.mp3`);
+    sound.play()
+}
+
+
+const createAnimation = (key) => {
+    let active = document.querySelector(`.${key}`);
+    active.classList.add("press");
+    
+    setTimeout(() => {
+        active.classList.remove("press");
+    }, 1000);
 }
